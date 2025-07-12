da4f0e73a168407f1afca71591b53c688a4f43c8
MohakGupta2004 <mohakgupta500@gmail.com>
2025-07-13 03:15:51 +0530

    code: balance show feature add

--- src/Airdrop.tsx
+++ src/Airdrop.tsx
@@ -1,14 +1,28 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 export const Airdrop = () => {
   const wallet = useWallet()
   const { connection } = useConnection()
   const [amount, setAmount] = useState(0)
   const [signature, setSignature] = useState("")
+  const [balance, setBalance] = useState(0)
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      if (!wallet.publicKey) {
+        return
+      }
+      const balance = await connection.getBalance(wallet.publicKey)
+      setBalance(balance / LAMPORTS_PER_SOL)
+    }
+    fetchBalance()
+  }, [wallet.publicKey, connection]);
   return (
     <div>
       <h1>Airdrop for {wallet.publicKey?.toString()}</h1>
+      <p>User Balance: {balance}</p>
+      <p>please enter the amount of SOL you want to request:</p>
       <input type="text" placeholder="Enter your Amount" onChange={(e) => setAmount(Number(e.target.value) * LAMPORTS_PER_SOL)} />
       <button onClick={async () => {
         if (!wallet.publicKey) {
